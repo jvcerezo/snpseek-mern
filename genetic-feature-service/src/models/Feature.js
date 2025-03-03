80e07f292338cc35d65e2cdc207775143b20d3e8
jvcerezo <jvcerezo@up.edu.ph>
2025-03-03 12:53:17 +0800

    added feature model

---
+++ genetic-feature-service/src/models/Feature.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const featureSchema = new mongoose.Schema({
+    id: { type: String, required: true, trim: true },
+    geneName: { type: String, required: true, trim: true },
+    referenceGenome: { type: String, required: true, trim: true },
+    start: { type: Number, required: true },
+    end: { type: Number, required: true },
+    contig: { type: String, required: true, trim: true },
+    strand: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+}, {
+    timestamps: true
+});
+
+const Feature = mongoose.model("Feature", featureSchema);
+export default Feature;
